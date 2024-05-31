4891f229f36ee9f2febf7feb120a3fc6ee577811
Frederick C. Lee <frederick.c.lee@gmail.com>
2024-05-31 15:16:36 -0700

    Update Swift to v5.9 & MacOS v14.0
    Added MacOS v14.0 platform.

--- Package.swift
+++ Package.swift
@@ -1,10 +1,13 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "DcmSwift",
+    platforms: [
+        .macOS(.v14)
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
